b14d408d8d641a122487c88b414378e291c3b752
Ailee <ailee945@outlook.com>
2022-05-24 08:54:42 +0800

    feat(element-plus): 引入element-plus

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <LoginTest></LoginTest>
   </div>
 </template>
+<script setup>
+import LoginTest from './login/LoginTest.vue';
+</script>
